56dde443780e3258620f1fa1fd1919a4d004997c
tpevahouse25 <tpevahouse@pcsdv.com>
2024-01-07 17:48:44 -0600

    Removed streak dates being connected.

--- lib/src/ui/calendar_dates_section/gridview_elements.dart
+++ lib/src/ui/calendar_dates_section/gridview_elements.dart
@@ -1,15 +1,9 @@
 import 'package:clean_calendar/src/models/calendar_properties.dart';
 import 'package:clean_calendar/src/utils/get_suitable_calendar_general_date_widget.dart';
-import 'package:clean_calendar/src/utils/get_suitable_calendar_streak_date_widget.dart';
 import 'package:flutter/material.dart';
 
 class CalendarDateWidget extends StatelessWidget {
-  const CalendarDateWidget(
-      {Key? key,
-      required this.calendarProperties,
-      required this.pageViewElementDate,
-      required this.pageViewDate})
-      : super(key: key);
+  const CalendarDateWidget({Key? key, required this.calendarProperties, required this.pageViewElementDate, required this.pageViewDate}) : super(key: key);
 
   final CalendarProperties calendarProperties;
   final DateTime pageViewElementDate;
@@ -18,10 +12,7 @@ class CalendarDateWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (calendarProperties.datesForStreaks.contains(pageViewElementDate)) {
-      return GetSuitableCalendarStreakDateWidget(
-          calendarProperties: calendarProperties,
-          pageViewElementDate: pageViewElementDate,
-          pageViewDate: pageViewDate);
+      return GetSuitableCalendarGeneralDateWidget(calendarProperties: calendarProperties, pageViewElementDate: pageViewElementDate, pageViewDate: pageViewDate);
     }
     if (!calendarProperties.datesForStreaks.contains(pageViewElementDate)) {
       return GetSuitableCalendarGeneralDateWidget(
